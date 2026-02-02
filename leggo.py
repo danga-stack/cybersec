902587378b29b9831fd5aead24201fd0da51c23a
danga-stack <danielegagliardi116@gmail.com>
2026-02-02 10:23:42 +0100

    Update leggo.py

--- leggo.py
+++ leggo.py
@@ -1,1 +1,13 @@
-#ciao ciao
+def main():
+
+  nomefile = "logfile.txt"
+  try:
+    f = open(nomefile, "r", encoding = "utf-8")
+    righe = f.readlines()
+    for riga in righe:
+      print(riga, end ="")
+  except FileNotFoundError as e:
+    print(f"[-] errore bloccante: {str(e)}")
+
+if __name__ == "__main__":
+  main()
